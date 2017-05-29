12dca1f0022c77731005a390b6e1d14e4f6e2e71
TJ Draper <tj@buzzingpixel.com>
2017-05-29 09:38:05 -0500

    Create pings table

---
+++ database/migrations/2017_05_29_142950_create_pings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/** @noinspection AutoloadingIssuesInspection */
+/**
+ * Class CreatePingsTable
+ */
+class CreatePingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('guid')->nullable(false);
+            $table->string('name');
+            $table->timestamp('expect_every');
+            $table->timestamp('warn_after');
+            $table->timestamp('last_ping');
+            $table->boolean('has_error')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pings');
+    }
+}
